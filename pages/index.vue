e1a8b431b0e3cc734f12667c5d00d5c594875b16
mk_limitbreak <matsuoka812930@gmail.com>
2019-04-03 20:30:41 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div>
+      <div class="container" style="padding-left: 5%;padding-right: 5%">
+        <div class="column">
+          <label class="subtitle" style="">Search</label>
+        </div>
+        <div class="field">
+          <input class="input" v-model="algoliaResults.name"/>
+        </div>
+        <div class="container">
+          <div class="columns is-mobile is-multiline is-centered">
+            <div class="column is-narrow" v-for="card in algoliaResults.data" :key="card.id">
+              <figure class="image" style="width: 111px;height: 155px">
+                <img :src="cardImage(card)">
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br>
+    </div>
+  </div>
+</template>
+<script>
+  import algoliasearch from 'algoliasearch'
+
+  export default {
+    data() {
+      return {
+        algoliaResults: {
+          data: [],
+          name: ''
+        }
+      }
+    },
+    methods: {
+      cardImage(card) {
+        return 'http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=' + card.multiverseid + '&type=card'
+      }
+    },
+    watch: {
+      'algoliaResults.name': function () {}
+    }
+  }
+</script>
---
+++ plugins/buefy.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+Vue.component(Buefy.Field.name, Buefy.Field)
+Vue.component(Buefy.Autocomplete.name, Buefy.Autocomplete)
+
+
